1f5d75d6d0bdffaabb2e05a762f5d0e9359c103d
㆔ MaxMa <shop.maxma@gmail.com>
2024-02-05 10:31:22 +0700

    feat: update config genesis

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-rm -Rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
+rm -Rf ./consensus/beacondata ./consensus/validatordata ./config/genesis.ssz
 rm -Rf ./consensus/beacondata-lighthouse ./consensus/validatordata-lighthouse
 rm -Rf ./execution/geth
 rm -Rf ./blockscout/blockscout-db-data ./blockscout/stats-db-data ./blockscout/redis-data ./blockscout/logs ./blockscout/tmp ./blockscout/postgres-data
--- lighthouse/import-validator.sh
+++ lighthouse/import-validator.sh
@@ -1,1 +1,7 @@
-docker run -it --rm -v $PWD/../consensus/validatordata-lighthouse:/root/lighthouse -v $PWD/../consensus/validatordata-lighthouse/custom/validators:/root/.lighthouse/custom/validators -v $PWD/../config:/config -v ./validator_keys:/keys sigp/lighthouse lighthouse account validator import --directory=/keys --testnet-dir=/config --reuse-password
+docker run -it --rm \
+-v $PWD/../consensus/validatordata-lighthouse:/root/lighthouse \
+-v $PWD/../consensus/validatordata-lighthouse/custom/validators:/root/.lighthouse/custom/validators \
+-v $PWD/../config:/config \
+-v ./validator_keys:/keys \
+sigp/lighthouse \
+lighthouse account validator import --directory=/keys --testnet-dir=/config --reuse-password
